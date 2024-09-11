ad2484d179c03fa16d8ced9bdaef6700de90752a
Pavan20kumar <154593775+Pavan20kumar@users.noreply.github.com>
2024-09-11 22:05:32 +0530

    Update index.js

--- src/Component/display/index.js
+++ src/Component/display/index.js
@@ -18,7 +18,7 @@ function Display(){
 
    const handlDeletle = (id)=>{
     
-    axios.delete(`https://curd-backed-mysql.onrender.com//Delete/${id}`).then((res)=>{
+    axios.delete(`https://curd-backed-mysql.onrender.com/Delete/${id}`).then((res)=>{
         console.log(res);
         window.location.reload();
         navigate('/');
